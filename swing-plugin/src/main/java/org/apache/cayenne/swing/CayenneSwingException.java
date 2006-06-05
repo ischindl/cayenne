de14136d4cad2294f8cb0d0997a73628c3389ac9
Andrus Adamchik <aadamchik@apache.org>
2006-06-05 11:27:40 +0000

    cleanup of the plugin files
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cayenne/main/branches/PROTO-3.0@411749 13f79535-47bb-0310-9956-ffa450edef68

--- swing-plugin/src/main/java/org/apache/cayenne/swing/CayenneSwingException.java
+++ swing-plugin/src/main/java/org/apache/cayenne/swing/CayenneSwingException.java
@@ -1,20 +1,36 @@
+/*
+ *  Copyright 2006 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.apache.cayenne.swing;
 
+import org.objectstyle.cayenne.CayenneRuntimeException;
 
-public class CayenneSwingException extends RuntimeException {
+public class CayenneSwingException extends CayenneRuntimeException {
 
-	public CayenneSwingException() {
-	}
+    public CayenneSwingException() {
+    }
 
-	public CayenneSwingException(String message) {
-		super(message);
-	}
+    public CayenneSwingException(String message) {
+        super(message);
+    }
 
-	public CayenneSwingException(Throwable cause) {
-		super(cause);
-	}
+    public CayenneSwingException(Throwable cause) {
+        super(cause);
+    }
 
-	public CayenneSwingException(String message, Throwable cause) {
-		super(message, cause);
-	}
+    public CayenneSwingException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
