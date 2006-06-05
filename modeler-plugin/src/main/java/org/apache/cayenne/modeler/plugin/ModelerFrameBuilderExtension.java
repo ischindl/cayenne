4ddc45cdbb3823128f9203bd8804d40b30eb7a1e
Andrus Adamchik <aadamchik@apache.org>
2006-06-05 16:14:05 +0000

    checking CayenneModeler platonos plugin - still very basic and does not use the actual modeler code
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cayenne/main/branches/PROTO-3.0@411844 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modeler-plugin/src/main/java/org/apache/cayenne/modeler/plugin/ModelerFrameBuilderExtension.java
@@ -0,0 +1,49 @@
+/*
+ *  Copyright 2006 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.cayenne.modeler.plugin;
+
+import org.apache.cayenne.swing.plugin.frame.FrameBuilderExtension;
+import org.apache.cayenne.swing.plugin.frame.FramePlugin;
+import org.platonos.pluginengine.Plugin;
+import org.platonos.pluginengine.PluginEngine;
+
+/**
+ * An extension that turns a generic frame provided by swing-plugin to a CayenneModeler
+ * frame.
+ * 
+ * @author Andrus Adamchik
+ */
+public class ModelerFrameBuilderExtension implements FrameBuilderExtension {
+
+    protected Plugin plugin;
+
+    public ModelerFrameBuilderExtension() {
+        this.plugin = PluginEngine.getPlugin(ModelerPlugin.class);
+    }
+
+    public void initActions(FramePlugin plugin) {
+
+    }
+
+    public void initFrame(FramePlugin plugin) {
+        plugin.getFrameController().getFrame().setTitle(
+                this.plugin.replaceToken("$$frame.title$$"));
+    }
+
+    public void initMenus(FramePlugin plugin) {
+        plugin.getMenuBuilder().addMenu(this.plugin, "menu.xml");
+    }
+}
---
+++ modeler-plugin/src/main/java/org/apache/cayenne/modeler/plugin/ModelerPlugin.java
@@ -0,0 +1,7 @@
+package org.apache.cayenne.modeler.plugin;
+
+import org.platonos.pluginengine.PluginLifecycle;
+
+public class ModelerPlugin extends PluginLifecycle {
+
+}
