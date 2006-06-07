3ca0a3f8f999c323ca4ea127f6f1f4d0d64e2547
Andrus Adamchik <aadamchik@apache.org>
2006-06-07 11:50:33 +0000

    script-based launcher
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cayenne/main/branches/PROTO-3.0@412377 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modeler/plugin-launcher/src/main/bin/modeler.sh
@@ -0,0 +1,83 @@
+#!/bin/sh
+
+# Bourne shell script to start Cayenne Modeler.
+#
+# Certain parts are modeled after Tomcat startup scrips, 
+# Copyright Apache Software Foundation
+
+MAIN_CLASS=Launcher
+
+CLASSPATH=
+
+# OS specific support.
+cygwin=false
+case "`uname`" in
+CYGWIN*) cygwin=true;;
+esac
+
+PATH_SEPARATOR=:
+if [ "$cygwin" = "true" ] ; then 
+	PATH_SEPARATOR=";"
+fi
+
+# Guess from startup directory
+# if [ "$CAYENNE_HOME" = "" ] ; then
+	# resolve links - $0 may be a softlink
+	PRG="$0"
+
+	while [ -h "$PRG" ] ; do
+  		ls=`ls -ld "$PRG"`
+  		link=`expr "$ls" : '.*-> \(.*\)$'`
+  		if expr "$link" : '.*/.*' > /dev/null; then
+    		PRG="$link"
+  		else
+			PRG=`dirname "$PRG"`/"$link"
+		fi
+	done
+ 
+	CAYENNE_HOME=`dirname "$PRG"`
+#fi
+
+
+if [ ! -f $CAYENNE_HOME/modeler.sh ] ; then
+    echo "Please define CAYENNE_HOME to point to your Cayenne installation."
+    exit 1
+fi
+
+# Guess Java location from PATH or from JAVA_HOME
+if [ "$JAVA_HOME" = "" ] ; then 
+	JAVACMD=java
+else
+	JAVACMD=$JAVA_HOME/bin/java
+	if [ ! -f $JAVACMD ] ; then
+		JAVACMD=$JAVA_HOME/jre/bin/java
+	fi
+fi
+
+CAYENNE_MODELER_JAR_PATH="`ls $CAYENNE_HOME/lib/plugin-launcher*.jar`"
+echo $CAYENNE_MODELER_JAR_PATH
+if [ "$cygwin" = "true" ] ; then
+    CAYENNE_CLASSPATH=`cygpath -w $CAYENNE_MODELER_JAR_PATH`
+else 
+    CAYENNE_CLASSPATH=$CAYENNE_MODELER_JAR_PATH
+fi
+    
+OPTIONS="-classpath $CAYENNE_CLASSPATH"
+PROPERTIES="-Dplatonos.plugins.dirs=$CAYENNE_HOME/plugins"
+
+# Mac OS X Specific properties
+if [ -d /System/Library/Frameworks/JavaVM.framework ]; then
+  PROP1="-Dcom.apple.mrj.application.apple.menu.about.name=CayenneModeler"
+  PROP2="-Dapple.laf.useScreenMenuBar=true"
+  PROP3="-Xdock:name=CayenneModeler"
+  PROP4="-Xdock:icon=${CAYENNE_HOME}/bin/icon.ico"
+
+  PROPERTIES="$PROPERTIES $PROP1 $PROP2 $PROP3 $PROP4"
+fi
+
+# Start the Modeler
+CMD="$JAVACMD $OPTIONS $PROPERTIES $MAIN_CLASS $@ &"
+echo $CMD
+echo
+
+$CMD
---
+++ modeler/plugin-launcher/src/main/java/Launcher.java
@@ -0,0 +1,81 @@
+
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+import org.platonos.pluginengine.Plugin;
+import org.platonos.pluginengine.PluginEngine;
+import org.platonos.pluginengine.logging.LoggerLevel;
+
+/**
+ * Starts a plugin shell for a Swing application.
+ */
+public class Launcher {
+
+    /**
+     * A property specifying a location of the plugins directory. It can be a comma
+     * separated list of directories. If not set, {@link #PLUGINS_DIR_DEFAULT} directory
+     * is used.
+     */
+    public static final String PLUGINS_DIR_PROPERTY = "platonos.plugins.dirs";
+    public static final String PLUGINS_DIR_DEFAULT = "plugins";
+
+    /**
+     * A property specifying the name of the PluginEngine (same as the name of the
+     * application). If not set, {@link #ENGINE_NAME_DEFAULT} directory is used.
+     */
+    public static final String ENGINE_NAME_PROPERTY = "platonos.engine.name";
+    public static final String ENGINE_NAME_DEFAULT = "Platonos Plugin Engine";
+
+    public static void main(String[] args) {
+
+        String engineName = System.getProperty(ENGINE_NAME_PROPERTY);
+        if (engineName == null) {
+            engineName = ENGINE_NAME_DEFAULT;
+        }
+
+        PluginEngine pluginEngine = new PluginEngine(engineName);
+        pluginEngine.setAttribute(PluginEngine.COMMAND_LINE_ATTRIBUTE, args);
+
+        // load plugins...
+        String pluginDirectories = System.getProperty(PLUGINS_DIR_PROPERTY);
+
+        if (pluginDirectories == null) {
+            File defaultDir = new File(PLUGINS_DIR_DEFAULT);
+            if (defaultDir.isDirectory()) {
+                pluginDirectories = defaultDir.getAbsolutePath();
+            }
+        }
+
+        if (pluginDirectories != null) {
+            StringTokenizer toks = new StringTokenizer(pluginDirectories, ",");
+            while (toks.hasMoreTokens()) {
+                pluginEngine.loadPlugins(toks.nextToken());
+            }
+        }
+
+        pluginEngine.start();
+
+        boolean hasStartedPlugins = false;
+        Iterator it = pluginEngine.getPlugins().iterator();
+        while (it.hasNext()) {
+            Plugin p = (Plugin) it.next();
+            if (p.isStarted()) {
+                hasStartedPlugins = true;
+                break;
+            }
+        }
+
+        // either no plugins configured, all all of them failed to start.
+        if (!hasStartedPlugins) {
+            pluginEngine.getLogger().log(
+                    LoggerLevel.INFO,
+                    "No plugins started, exiting",
+                    null);
+
+            // must explicitly kill all UI threads
+            System.exit(0);
+        }
+    }
+}
