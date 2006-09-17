9a7f813b170799cdf11cb636bb2ee77e2fb0f33b
Andrus Adamchik <aadamchik@apache.org>
2006-09-17 22:12:23 +0000

    CAY-654 - 2.0 branch port of DBCPDataSourceFactory
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cayenne/main/branches/STABLE-2.0@447160 13f79535-47bb-0310-9956-ffa450edef68

--- cayenne/cayenne-java/src/tests/java/org/apache/cayenne/conf/DBCPDataSourcePropertiesTst.java
+++ cayenne/cayenne-java/src/tests/java/org/apache/cayenne/conf/DBCPDataSourcePropertiesTst.java
@@ -22,101 +22,95 @@ package org.apache.cayenne.conf;
 import java.sql.Connection;
 import java.util.Properties;
 
-import org.apache.commons.pool.impl.GenericObjectPool;
-import org.apache.cayenne.ConfigurationException;
-
 import junit.framework.TestCase;
 
+import org.apache.cayenne.ConfigurationException;
+import org.apache.commons.pool.impl.GenericObjectPool;
+
 /**
- * @author Andrei Adamchik
+ * @author Andrus Adamchik
  */
-public class DBCPDataSourceFactoryTst extends TestCase {
+public class DBCPDataSourcePropertiesTst extends TestCase {
 
     public void testStringProperty() {
-        DBCPDataSourceFactory factory = new DBCPDataSourceFactory();
         Properties props = new Properties();
         props.put("a", "X");
         props.put("cayenne.dbcp.c", "Y");
+        DBCPDataSourceProperties factory = new DBCPDataSourceProperties(props);
 
-        assertNull(factory.stringProperty(props, "a"));
-        assertNull(factory.stringProperty(props, "b"));
-        assertEquals("Y", factory.stringProperty(props, "c"));
+        assertNull(factory.getString("a"));
+        assertNull(factory.getString("b"));
+        assertEquals("Y", factory.getString("c"));
     }
 
     public void testIntProperty() {
-        DBCPDataSourceFactory factory = new DBCPDataSourceFactory();
+
         Properties props = new Properties();
         props.put("a", "10");
         props.put("cayenne.dbcp.b", "11");
         props.put("cayenne.dbcp.d", "**");
+        DBCPDataSourceProperties factory = new DBCPDataSourceProperties(props);
 
-        assertEquals(11, factory.intProperty(props, "b", -1));
-        assertEquals(-1, factory.intProperty(props, "a", -1));
-        assertEquals(-1, factory.intProperty(props, "c", -1));
-        assertEquals(-2, factory.intProperty(props, "d", -2));
+        assertEquals(11, factory.getInt("b", -1));
+        assertEquals(-1, factory.getInt("a", -1));
+        assertEquals(-1, factory.getInt("c", -1));
+        assertEquals(-2, factory.getInt("d", -2));
     }
 
     public void testWhenExhaustedAction() throws Exception {
-        DBCPDataSourceFactory factory = new DBCPDataSourceFactory();
         Properties props = new Properties();
         props.put("cayenne.dbcp.a", "1");
         props.put("cayenne.dbcp.b", "WHEN_EXHAUSTED_BLOCK");
         props.put("cayenne.dbcp.c", "WHEN_EXHAUSTED_GROW");
         props.put("cayenne.dbcp.d", "WHEN_EXHAUSTED_FAIL");
         props.put("cayenne.dbcp.e", "garbage");
+        DBCPDataSourceProperties factory = new DBCPDataSourceProperties(props);
 
-        assertEquals(1, factory.whenExhaustedAction(props, "a", (byte) 100));
-        assertEquals(GenericObjectPool.WHEN_EXHAUSTED_BLOCK, factory.whenExhaustedAction(
-                props,
-                "b",
-                (byte) 100));
-        assertEquals(GenericObjectPool.WHEN_EXHAUSTED_GROW, factory.whenExhaustedAction(
-                props,
-                "c",
-                (byte) 100));
-        assertEquals(GenericObjectPool.WHEN_EXHAUSTED_FAIL, factory.whenExhaustedAction(
-                props,
-                "d",
-                (byte) 100));
+        assertEquals(1, factory.getWhenExhaustedAction("a", (byte) 100));
+        assertEquals(GenericObjectPool.WHEN_EXHAUSTED_BLOCK, factory
+                .getWhenExhaustedAction("b", (byte) 100));
+        assertEquals(GenericObjectPool.WHEN_EXHAUSTED_GROW, factory
+                .getWhenExhaustedAction("c", (byte) 100));
+        assertEquals(GenericObjectPool.WHEN_EXHAUSTED_FAIL, factory
+                .getWhenExhaustedAction("d", (byte) 100));
 
         try {
-            factory.whenExhaustedAction(props, "e", (byte) 100);
+            factory.getWhenExhaustedAction("e", (byte) 100);
             fail("must throw on invalid key");
         }
         catch (ConfigurationException ex) {
             // expected
         }
 
-        assertEquals(100, factory.whenExhaustedAction(props, "f", (byte) 100));
+        assertEquals(100, factory.getWhenExhaustedAction("f", (byte) 100));
     }
 
     public void testTransactionIsolation() throws Exception {
-        DBCPDataSourceFactory factory = new DBCPDataSourceFactory();
         Properties props = new Properties();
         props.put("cayenne.dbcp.a", "1");
         props.put("cayenne.dbcp.b", "TRANSACTION_NONE");
         props.put("cayenne.dbcp.c", "TRANSACTION_READ_UNCOMMITTED");
         props.put("cayenne.dbcp.d", "TRANSACTION_SERIALIZABLE");
         props.put("cayenne.dbcp.e", "garbage");
-
-        assertEquals(1, factory.defaultTransactionIsolation(props, "a", (byte) 100));
-        assertEquals(Connection.TRANSACTION_NONE, factory.defaultTransactionIsolation(
-                props,
+        DBCPDataSourceProperties factory = new DBCPDataSourceProperties(props);
+        assertEquals(1, factory.getTransactionIsolation("a", (byte) 100));
+        assertEquals(Connection.TRANSACTION_NONE, factory.getTransactionIsolation(
                 "b",
                 (byte) 100));
+
         assertEquals(Connection.TRANSACTION_READ_UNCOMMITTED, factory
-                .defaultTransactionIsolation(props, "c", (byte) 100));
+                .getTransactionIsolation("c", (byte) 100));
         assertEquals(Connection.TRANSACTION_SERIALIZABLE, factory
-                .defaultTransactionIsolation(props, "d", (byte) 100));
+                .getTransactionIsolation("d", (byte) 100));
 
         try {
-            factory.defaultTransactionIsolation(props, "e", (byte) 100);
+            factory.getTransactionIsolation("e", (byte) 100);
             fail("must throw on invalid key");
         }
         catch (ConfigurationException ex) {
             // expected
         }
 
-        assertEquals(100, factory.defaultTransactionIsolation(props, "f", (byte) 100));
+        assertEquals(100, factory.getTransactionIsolation("f", (byte) 100));
     }
 }
