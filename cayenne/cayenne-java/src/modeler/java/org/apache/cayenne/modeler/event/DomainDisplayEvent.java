dbbfa2effd7a2422923a74a76e44673a837dc7aa
Andrus Adamchik <aadamchik@apache.org>
2006-09-19 20:45:31 +0000

    missing licenses - 2.0
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cayenne/main/branches/STABLE-2.0@447960 13f79535-47bb-0310-9956-ffa450edef68

--- cayenne/cayenne-java/src/modeler/java/org/apache/cayenne/modeler/event/DomainDisplayEvent.java
+++ cayenne/cayenne-java/src/modeler/java/org/apache/cayenne/modeler/event/DomainDisplayEvent.java
@@ -1,4 +1,3 @@
-package org.apache.cayenne.modeler.event;
 /*****************************************************************
  *   Licensed to the Apache Software Foundation (ASF) under one
  *  or more contributor license agreements.  See the NOTICE file
@@ -17,7 +16,7 @@ package org.apache.cayenne.modeler.event;
  *  specific language governing permissions and limitations
  *  under the License.
  ****************************************************************/
-
+package org.apache.cayenne.modeler.event;
 
 import org.apache.cayenne.access.DataDomain;
 
